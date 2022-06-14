846a7403645c5564ebbeb8a58d893a2da1fd494f
nazimuddin <nazimuddin5390@gmail.com>
2022-06-14 12:07:35 +0600

    selecation statements

---
+++ selecation_statements.java
@@ -0,0 +1,12 @@
+public class selecation_statements {
+    public static void main(String [] args){
+        int var1 = 5;
+        int var2 = 6;
+        if((var2=1)==var1){
+            System.out.println(var2);
+        }
+        else{
+            System.out.println(++var2);
+        }
+    }
+}
